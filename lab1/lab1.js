2e7b1cd363acc4ee8776366d14c3792aef2e99e5
My Le <mylhtpd08034@fpt.edu.vn>
2023-03-10 17:43:29 +0700

    nop lab 1

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,4 +1,8 @@
-// nhap a tu ban phim 
-let a = prompt("Nhap so nguyen a :");
-// nhap b tu ban phim 
-let b = prompt ("Nhao so nguyen b :");
+//nhap a tu ban phim 
+let a1 = parselnt 
+//nhap b tu ban phim 
+let b = prompt ("nhap so nguyen b :")
+//let b1 = parselnt 
+let c = parselnt(a)+parselnt(b);
+document.write (c);
+cốnle.log(c) 
